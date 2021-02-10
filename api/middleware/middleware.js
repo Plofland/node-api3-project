4f5227d695f168f78078b14927a15adabdb35f8d
Plofland <ploflan@gmail.com>
2021-02-10 18:38:09 -0500

    Same as last commit description

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,24 @@
+const postFunc = require('../posts/posts-model')
+
 function logger(req, res, next) {
-  // do your magic!
+  console.log(`A ${req.method} request was made at ${Date().toLocalString} on ${req.originalUrl}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const {id} = req.params;
+  const postId = postFunc.getById(id);
+  try {
+    if(!id){
+      res.status(400).json({message: `No post with the id of ${id}`})
+    } else {
+      req.postId = postId;
+      next();
+    }
+  } catch (error) {
+    res.status(500).json({`Server error: ${error}`})
+  }
+
 }
 
 function validateUser(req, res, next) {
@@ -15,4 +30,4 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
-// module.exports = { logger, validateUserId, validateUser, validatePost };
+module.exports = { logger, validateUserId, validateUser, validatePost };
--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const postsFunctions = require('./posts-model')
+const { logger } = require('../middleware/middleware.js');
+const postFunc = require('./posts-model.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE POSTS
-  
+  // logger();
+  postFunc
+    .get()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Error retrieving the posts' });
+    });
 });
 
 router.get('/:id', (req, res) => {
@@ -13,5 +21,4 @@ router.get('/:id', (req, res) => {
   // this needs a middleware to verify post id
 });
 
-// do not forget to export the router
 module.exports = router;
